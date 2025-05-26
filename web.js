7abdf5aca174ea69ed80675255309e16be37b72d
MalenaHuy <60413675+MalenaHuy@users.noreply.github.com>
2025-05-25 23:21:52 -0500

    Update web.js
    second

--- web.js
+++ web.js
@@ -15,7 +15,7 @@ var counter = 1;
 // want to run something if I click; everyting the the {} will runs if i click
 btn.addEventListener("click", function(){
     // Loop from 0 to 6; only run if counter is exactly 6
-    if (counter ===6){
+    if (counter ===3){
         counter = 0;
     }
     // alert("clicked!")
